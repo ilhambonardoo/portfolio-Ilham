b9a0bcc68679907584fc031828dceb69dc7e10ff
ilhambonardoo <ilhambonardo123@gmail.com>
2025-08-05 14:47:05 +0700

    change class to className

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,47 +7,47 @@ import boxing from "../assets/boxing.jpg";
 export function Experience() {
   return (
     <>
-      <section id="portfolio" class="my-16 mb-0 bg-gradient-to-b from-white to-stone-200 shadow-2xl">
-        <div class="container mx-auto">
-          <div class="text-center pt-5">
-            <h2 class="text-2xl font-semibold inter my-3">Portfolio</h2>
-            <h3 class="text-lg font-semibold inter text-stone-500 mt-3">My Experience</h3>
+      <section id="portfolio" className="my-16 mb-0 bg-gradient-to-b from-white to-stone-200 shadow-2xl">
+        <div className="container mx-auto">
+          <div className="text-center pt-5">
+            <h2 className="text-2xl font-semibold inter my-3">Portfolio</h2>
+            <h3 className="text-lg font-semibold inter text-stone-500 mt-3">My Experience</h3>
           </div>
-          <div class="flex flex-wrap justify-center mx-5 lg:mx-auto lg:w-10/12">
-            <div class="mb-12 p-7 shadow-lg hover:shadow-2xl md:w-1/2 rounded-lg" data-aos="fade-right" data-aos-offset="400" data-aos-easing="ease-in-sine">
-              <div class="py-7">
-                <img src={aplikasiMobile} alt="" class="w-full rounded-2xl" />
+          <div className="flex flex-wrap justify-center mx-5 lg:mx-auto lg:w-10/12">
+            <div className="mb-12 p-7 shadow-lg hover:shadow-2xl md:w-1/2 rounded-lg" data-aos="fade-right" data-aos-offset="400" data-aos-easing="ease-in-sine">
+              <div className="py-7">
+                <img src={aplikasiMobile} alt="" className="w-full rounded-2xl" />
               </div>
-              <h2 class="font-semibold text-xl">Monitoring Suhu</h2>
-              <p class="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
+              <h2 className="font-semibold text-xl">Monitoring Suhu</h2>
+              <p className="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
             </div>
-            <div class="mb-12 p-7 shadow-2xl hover:shadow-lg md:w-1/2 rounded-lg cursor-pointer" data-aos="fade-left" data-aos-offset="400" data-aos-easing="ease-in-sine">
-              <div class="py-7">
-                <img src={basket} alt="" class="w-full rounded-2xl" />
+            <div className="mb-12 p-7 shadow-2xl hover:shadow-lg md:w-1/2 rounded-lg cursor-pointer" data-aos="fade-left" data-aos-offset="400" data-aos-easing="ease-in-sine">
+              <div className="py-7">
+                <img src={basket} alt="" className="w-full rounded-2xl" />
               </div>
-              <h2 class="font-semibold text-xl">Juara 1 OMI Basket Putra</h2>
-              <p class="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
+              <h2 className="font-semibold text-xl">Juara 1 OMI Basket Putra</h2>
+              <p className="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
             </div>
-            <div class="mb-12 p-7 shadow-2xl hover:shadow-lg md:w-1/2 rounded-lg " data-aos="fade-up" data-aos-duration="3000">
-              <div class="py-7">
-                <img src={radig} alt="" class="w-full rounded-2xl" />
+            <div className="mb-12 p-7 shadow-2xl hover:shadow-lg md:w-1/2 rounded-lg " data-aos="fade-up" data-aos-duration="3000">
+              <div className="py-7">
+                <img src={radig} alt="" className="w-full rounded-2xl" />
               </div>
-              <h2 class="font-semibold text-xl">Smart Parking Light</h2>
-              <p class="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
+              <h2 className="font-semibold text-xl">Smart Parking Light</h2>
+              <p className="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
             </div>
-            <div class="mb-12 p-7 shadow-lg hover:shadow-2xl md:w-1/2 rounded-lg" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
-              <div class="py-7">
-                <img src={psd} alt="" class="w-full rounded-2xl" />
+            <div className="mb-12 p-7 shadow-lg hover:shadow-2xl md:w-1/2 rounded-lg" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
+              <div className="py-7">
+                <img src={psd} alt="" className="w-full rounded-2xl" />
               </div>
-              <h2 class="font-semibold text-xl">Smart Feeder</h2>
-              <p class="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
+              <h2 className="font-semibold text-xl">Smart Feeder</h2>
+              <p className="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
             </div>
-            <div class="mb-12 p-7 shadow-2xl md:w-1/2 rounded-lg" data-aos="zoom-in" data-aos-duration="1500">
-              <div class="py-7">
-                <img src={boxing} alt="" class="w-full rounded-2xl" />
+            <div className="mb-12 p-7 shadow-2xl md:w-1/2 rounded-lg" data-aos="zoom-in" data-aos-duration="1500">
+              <div className="py-7">
+                <img src={boxing} alt="" className="w-full rounded-2xl" />
               </div>
-              <h2 class="font-semibold text-xl">Gold medal Boxing</h2>
-              <p class="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
+              <h2 className="font-semibold text-xl">Gold medal Boxing</h2>
+              <p className="text-base font-medium text-stone-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti reprehenderit assumenda dicta magni fuga consectetur suscipit commodi voluptatum nobis nisi?</p>
             </div>
           </div>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,56 +1,56 @@
 export function Footer() {
   return (
     <>
-      <footer class="bg-stone-800 pt-24 pb-12">
-        <div class="container md:mx-auto">
-          <div class="flex flex-wrap lg:pl-35 r-20">
-            <div class="w-full px-14 mb-12 text-slate-300 font-medium md:w-1/3">
-              <h2 class="font-bold text-white mb-4 text-4xl">SixDecFour</h2>
-              <h3 class="font-bold text-2xl mb-4">Contact Person</h3>
+      <footer className="bg-stone-800 pt-24 pb-12">
+        <div className="container md:mx-auto">
+          <div className="flex flex-wrap lg:pl-35 r-20">
+            <div className="w-full px-14 mb-12 text-slate-300 font-medium md:w-1/3">
+              <h2 className="font-bold text-white mb-4 text-4xl">SixDecFour</h2>
+              <h3 className="font-bold text-2xl mb-4">Contact Person</h3>
               <p>ilhambonardo123@gmail.com</p>
               <p>Jl.Makassar Sydney no.114</p>
               <p>America</p>
             </div>
-            <div class="w-full px-14 mb-12 md:w-1/3 text-slate-300">
-              <h3 class="font-bold text-white mb-4 text-2xl">Social Media</h3>
+            <div className="w-full px-14 mb-12 md:w-1/3 text-slate-300">
+              <h3 className="font-bold text-white mb-4 text-2xl">Social Media</h3>
               <ul>
                 <li>
-                  <a href="https://www.instagram.com/ilhambonardoo/" class="inline-block text-base hover:text-stone-400">
+                  <a href="https://www.instagram.com/ilhambonardoo/" className="inline-block text-base hover:text-stone-400">
                     Instagram
                   </a>
                 </li>
                 <li>
-                  <a href="https://x.com/JackRich84050" class="inline-block text-base hover:text-stone-400">
+                  <a href="https://x.com/JackRich84050" className="inline-block text-base hover:text-stone-400">
                     Twitter
                   </a>
                 </li>
                 <li>
-                  <a href="https://github.com/ilhambonardoo" class="inline-block text-base hover:text-stone-400">
+                  <a href="https://github.com/ilhambonardoo" className="inline-block text-base hover:text-stone-400">
                     Github
                   </a>
                 </li>
               </ul>
             </div>
-            <div class="w-full px-14 mb-12 md:w-1/3 text-slate-300">
-              <h3 class="font-bold text-white mb-4 text-2xl">Tautan</h3>
+            <div className="w-full px-14 mb-12 md:w-1/3 text-slate-300">
+              <h3 className="font-bold text-white mb-4 text-2xl">Tautan</h3>
               <ul>
                 <li>
-                  <a href="#home" class="inline-block text-base hover:text-stone-400">
+                  <a href="#home" className="inline-block text-base hover:text-stone-400">
                     Beranda
                   </a>
                 </li>
                 <li>
-                  <a href="#about" class="inline-block text-base hover:text-stone-400">
+                  <a href="#about" className="inline-block text-base hover:text-stone-400">
                     About
                   </a>
                 </li>
                 <li>
-                  <a href="#portfolio" class="inline-block text-base hover:text-stone-400">
+                  <a href="#portfolio" className="inline-block text-base hover:text-stone-400">
                     Portfolio
                   </a>
                 </li>
                 <li>
-                  <a href="#contact" class="inline-block text-base hover:text-stone-400">
+                  <a href="#contact" className="inline-block text-base hover:text-stone-400">
                     Contact
                   </a>
                 </li>
@@ -58,8 +58,8 @@ export function Footer() {
             </div>
           </div>
         </div>
-        <div class="w-full border-t border-stone-300">
-          <p class="text-center text-white font-semibold mt-7">© 2025 Ilham Bonardo Marpaung. All rights reserved.</p>
+        <div className="w-full border-t border-stone-300">
+          <p className="text-center text-white font-semibold mt-7">© 2025 Ilham Bonardo Marpaung. All rights reserved.</p>
         </div>
       </footer>
     </>
